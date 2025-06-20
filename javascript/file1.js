1ea03539ae4a83f3e0ec431ae2be0092faa26b70
ujjwaljha77 <jhaujjwal1970@gmail.com>
2025-06-20 10:04:36 +0530

    updated

--- javascript/file1.js
+++ javascript/file1.js
@@ -1,1 +1,19 @@
+// introduction to JavaScript
+
 console.log("hello world");
+let name="Ujjwal";
+let age=21; 
+let promoted=true;
+let score;
+let city=null;
+console.log(name, age, promoted, score, city);
+console.log(typeof name, typeof age, typeof promoted, typeof score, typeof city);
+let srtage= String(age);
+console.log(typeof srtage);
+let rating ="5.5";
+console.log(typeof rating);
+let strrating = Number(rating);
+console.log(typeof strrating);
+let flag="true";
+let isFlag = Boolean(flag);
+console.log(typeof isFlag);
---
+++ javascript/file2.js
@@ -0,0 +1,57 @@
+// operators in JavaScript
+// Arithmetic Operators
+
+
+
+// let a=54;
+// let b=10;
+
+// let c = a + b; //64
+// console.log(c);     
+
+// let d = a - b; //44
+// console.log(d);
+
+// let e = a * b; //540
+// console.log(e);
+
+// let f = a / b; //5.4
+// console.log(f);
+
+// let g = a % b;  //4
+// console.log(g);
+
+
+
+// let a="10";
+// let b=10;
+
+// console.log(a + b); //1010
+// console.log(a - b); //0
+// console.log(a * b); //100
+// console.log(a / b); //1
+// console.log(a % b); //0
+
+// let a=2;
+// let b=2;
+// console.log(a == b); // true
+// console.log(a != b); // false
+
+// let a="2";
+// let b=2;
+// console.log(a == b); // true
+// console.log(a === b); // false
+
+let a = 10
+let b=2
+let c = 12
+console.log(a>b && a>c); // false
+console.log(a>b || a>c); // true    
+console.log(a>b && a<c); // true
+console.log(a<b || a<c); // true
+console.log(!(a>b)); // false
+console.log(!(a<b)); // true
+
+console.log(a**b); // 100 (exponention)
+
+
